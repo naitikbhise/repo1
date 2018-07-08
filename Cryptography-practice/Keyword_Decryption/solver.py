4727d4ea7213e42f402a7d76e151b157820cd34a
Naitik Bhise <naitikbhise@gmail.com>
2018-07-08 22:50:35 +0530

    Update solver.py

--- Cryptography-practice/Keyword_Decryption/solver.py
+++ Cryptography-practice/Keyword_Decryption/solver.py
@@ -1,138 +1,89 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 11,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "lhpohes gvjhe ztytwojmmtel lgsfcgver segpsltjyl\n",
-      "Decrypted result:skulker choke minifloppies scratched recursions\n"
-     ]
-    }
-   ],
-   "source": [
-    "# Python program to check if two strings are isomorphic\n",
-    "MAX_CHARS = 256\n",
-    "\n",
-    "f = open('dictionary.lst', 'r')\n",
-    "data = f.read().lower()\n",
-    "#print(data)\n",
-    "f.close()\n",
-    "a = data.split()\n",
-    "count = {}\n",
-    "d = {}\n",
-    "# Isomorphic function\n",
-    "def areIsomorphic(string1, string2):\n",
-    "    m = len(string1)\n",
-    "    n = len(string2)\n",
-    " \n",
-    "    # Length of both strings must be same for one to one\n",
-    "    # corresponance\n",
-    "    if m != n:\n",
-    "        return False\n",
-    " \n",
-    "    # To mark visited characters in str2\n",
-    "    marked = [False] * MAX_CHARS\n",
-    " \n",
-    "    # To store mapping of every character from str1 to\n",
-    "    # that of str2. Initialize all entries of map as -1\n",
-    "    map = [-1] * MAX_CHARS\n",
-    " \n",
-    "    # Process all characters one by one\n",
-    "    for i in range(n):\n",
-    " \n",
-    "        # if current character of str1 is seen first\n",
-    "        # time in it.\n",
-    "        if map[ord(string1[i])] == -1:\n",
-    " \n",
-    "            # if current character of st2 is already\n",
-    "            # seen, one to one mapping not possible\n",
-    "            if marked[ord(string2[i])] == True:\n",
-    "                return False\n",
-    " \n",
-    "            # Mark current character of str2 as visited\n",
-    "            marked[ord(string2[i])] = True\n",
-    " \n",
-    "            # Store mapping of current characters\n",
-    "            map[ord(string1[i])] = string2[i]\n",
-    " \n",
-    "        # If this is not first appearance of current\n",
-    "        # character in str1, then check if previous\n",
-    "        # appearance mapped to same character of str2\n",
-    "        elif map[ord(string1[i])] != string2[i]:\n",
-    "            return False\n",
-    " \n",
-    "    return True\n",
-    "\n",
-    "print(\"Please provide the message to be decrypted:\")\n",
-    "w = input().split()\n",
-    "freqs = {}\n",
-    "for line in w:\n",
-    "    for char in line:\n",
-    "        if char in freqs:\n",
-    "            freqs[char] += 1\n",
-    "        else:\n",
-    "            freqs[char] = 1\n",
-    "#print(len(freqs))\n",
-    "b = \" \".join(w) \n",
-    "while(len(d)!=len(freqs)):\n",
-    "    for word in w:\n",
-    "        count[word]=0\n",
-    "        for wor in a:\n",
-    "            if areIsomorphic(word,wor):\n",
-    "                gue = 0\n",
-    "                for i in range(len(wor)):\n",
-    "                    if (word[i] in d and d[word[i]]!=wor[i]):\n",
-    "                        gue = 1\n",
-    "                if gue==0:\n",
-    "                    count[word]+=1\n",
-    "                    cor = wor\n",
-    "                \n",
-    "        if count[word] ==1:\n",
-    "            for i in range(len(word)):\n",
-    "                d[word[i]]=cor[i]\n",
-    "            w.remove(word)\n",
-    "            count.pop(word)\n",
-    "        #print(len(d))\n",
-    "d[' ']=' '\n",
-    "#print(len(d))\n",
-    "x = []\n",
-    "for i in range(len(b)):\n",
-    "    x.append(d[b[i]])\n",
-    "print(\"Decrypted result:\"+\"\".join(x))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.6.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+# Python program to check if two strings are isomorphic
+MAX_CHARS = 256
+
+f = open('dictionary.lst', 'r')
+data = f.read().lower()
+#print(data)
+f.close()
+a = data.split()
+count = {}
+d = {}
+# Isomorphic function
+def areIsomorphic(string1, string2):
+    m = len(string1)
+    n = len(string2)
+ 
+    # Length of both strings must be same for one to one
+    # corresponance
+    if m != n:
+        return False
+ 
+    # To mark visited characters in str2
+    marked = [False] * MAX_CHARS
+ 
+    # To store mapping of every character from str1 to
+    # that of str2. Initialize all entries of map as -1
+    map = [-1] * MAX_CHARS
+ 
+    # Process all characters one by one
+    for i in range(n):
+ 
+        # if current character of str1 is seen first
+        # time in it.
+        if map[ord(string1[i])] == -1:
+ 
+            # if current character of st2 is already
+            # seen, one to one mapping not possible
+            if marked[ord(string2[i])] == True:
+                return False
+ 
+            # Mark current character of str2 as visited
+            marked[ord(string2[i])] = True
+ 
+            # Store mapping of current characters
+            map[ord(string1[i])] = string2[i]
+ 
+        # If this is not first appearance of current
+        # character in str1, then check if previous
+        # appearance mapped to same character of str2
+        elif map[ord(string1[i])] != string2[i]:
+            return False
+ 
+    return True
+
+print("Please provide the message to be decrypted:")
+w = input().split()
+freqs = {}
+for line in w:
+    for char in line:
+        if char in freqs:
+            freqs[char] += 1
+        else:
+            freqs[char] = 1
+#print(len(freqs))
+b = " ".join(w) 
+while(len(d)!=len(freqs)):
+    for word in w:
+        count[word]=0
+        for wor in a:
+            if areIsomorphic(word,wor):
+                gue = 0
+                for i in range(len(wor)):
+                    if (word[i] in d and d[word[i]]!=wor[i]):
+                        gue = 1
+                if gue==0:
+                    count[word]+=1
+                    cor = wor
+                
+        if count[word] ==1:
+            for i in range(len(word)):
+                d[word[i]]=cor[i]
+            w.remove(word)
+            count.pop(word)
+        #print(len(d))
+d[' ']=' '
+#print(len(d))
+x = []
+for i in range(len(b)):
+    x.append(d[b[i]])
+print("Decrypted result:"+"".join(x))
